85bb89c73675e147b70e9085852eaf77a6d0d3af
Mello Birkan <mgbirkan@icloud.com>
2024-02-04 17:18:25 -0300

    Hashmap para ver o quao nova lingaugens de computação são, adicionado.

--- hashmap_languages/src/main.rs
+++ hashmap_languages/src/main.rs
@@ -16,9 +16,34 @@ fn init_languges() -> HashMap<String, i32> {
     languges.insert("PHP".to_owned(), 1995);
     languges.insert("PowerShell".to_owned(), 2006);
     languges.insert("Go".to_owned(), 2007);
+    languges.insert("Rust".to_owned(), 2010);
     languges
 }
 
+fn calculate_heights(years_active: &mut HashMap<String, i32>) -> HashMap<String, i32> {
+    for year in years_active.values_mut() {
+    *year = (*year - 2024).abs();
+    }
+
+    let min_year = *years_active.values().min().unwrap_or(&0);
+    let max_year = *years_active.values().max().unwrap_or(&0);
+
+    let mut weights: HashMap<String, i32> = HashMap::new();
+
+    for (lang, &year) in years_active.iter() {
+        let normalized_year = (year - min_year) as f64 / (max_year - min_year) as f64;
+        let weight = (normalized_year * 99.0) as i32 + 1;
+        weights.insert(lang.to_owned(), weight);
+    }
+    weights
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut languages = init_languges();
+    let weights = calculate_heights(&mut languages);
+
+    println!("Lnaguage weighing from 1-100 by age(1 is newest and 100 is oldest:");
+    for (lang, weight) in &weights {
+    println!("{}: {}", lang, weight);
+    }
 }
